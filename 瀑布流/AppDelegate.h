0238e870844ab65ec0f4328f88c28e2138261c8b
jh235 <jh235@qq.com>
2015-09-07 20:11:01 +0800

    Initial Commit

---
+++ 瀑布流/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  瀑布流
+//
+//  Created by jiang on 15/9/7.
+//  Copyright (c) 2015年 jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 瀑布流/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  瀑布流
+//
+//  Created by jiang on 15/9/7.
+//  Copyright (c) 2015年 jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
